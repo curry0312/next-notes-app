19f219dce3b71951ef9b51b3573924fafc62905d
WEI TSE <curry900312@gmail.com>
2023-06-20 15:32:57 +0800

    fix Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,31 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NextAuthProvider>
-          <Toaster
-            position="top-center"
-            reverseOrder={false}
-            gutter={8}
-            containerClassName=""
-            containerStyle={{}}
-            toastOptions={{
-              // Define default options
-              className: "",
-              duration: 5000,
-              style: {
-                background: "#363636",
-                color: "#fff",
-              },
-
-              // Default options for specific types
-              success: {
-                duration: 3000,
-                theme: {
-                  primary: "green",
-                  secondary: "black",
-                },
-              },
-            }}
-          />
+          <Toaster/>
           <Navbar />
           {children}
         </NextAuthProvider>
